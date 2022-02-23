82535c144951728bb53a30e93fa4e09e5fd03353
wangch <34704366@qq.com>
2022-02-23 10:56:22 +0800

    添加了设备页面

--- iiot-v3.0/pages/device/device.js
+++ iiot-v3.0/pages/device/device.js
@@ -1,66 +1,113 @@
+const app = getApp();
 // pages/device/device.js
 Page({
 
-  /**
-   * 页面的初始数据
-   */
   data: {
+    taskList:[],
+    taskListIndex: 0,
 
+    showModal: false,
+    machine_code: 'xxx',
+    title: '123'
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
+  
+  onShow: function (options) {
     // console.log("123");
+    this.getTaskList();
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
+  getTaskList() {
+    let that = this;
+    wx.request({
+      url: app.globalData.serverUrl + '/pda/suz/tasks'+ '/' +wx.getStorageSync('emp_code'),
+      header: {
+        "content-type" : "application/json",
+        "Authorization" : wx.getStorageSync('token_type')+" "+wx.getStorageSync('access_token')
+      },
+      method: "GET",
+      dataType: 'json',
+      success: (result) => {
+        console.log(result);
+
+        // http码
+        if(result.statusCode == 200) {
+          // 业务状态码
+          if (result.data.code == 200) {
+            // app.showSuccessToast("成功");
+
+            let list = [];
+            for (const item of result.data.data) {
+              item.onHide = 1;
+              list.push(item);
+            }
+            console.log(list);
+            that.setData({
+              taskList: list,
+            });
+            console.log(that.data.taskList);
+          } else {
+            // 业务码判断打印错误
+            app.processPostRequestConcreteCode(result.data.code, result.data.message);
+          }
+        } else {
+          // 对code码进行校验并且处理
+          app.processPostRequestStatusCode(result.statusCode);
+        }
+      },
+      fail: (res) => {
+        app.requestSendError(res);
+      },
+      complete: (res) => {},
+    })
+  },
+
+  // 展开折叠信息
+  unfold(event) {
+    // console.log(event);
+    const index = event.currentTarget.dataset.index;
+    let list = this.data.taskList;
+    // 将对应字典中的onHide标志位改变成0
+    list[index].onHide = 0;
+    this.setData({
+      taskList: list,
+    })
+  },
+
+  // 折叠信息
+  flod(event) {
+    // console.log(event);
+    const index = event.currentTarget.dataset.index;
+    let list = this.data.taskList;
+    // 将对应字典中的onHide标志位改变成0
+    list[index].onHide = 1;
+    this.setData({
+      taskList: list,
+    })
+  },
+
+  // 弹出modal框
+  popModal(event) {
+    // console.log(event)
+    const taskListIndex = event.currentTarget.dataset.index;
+    // console.log(taskListIndex)
+    this.setData({
+      showModal: true,
+      taskListIndex: taskListIndex,
+      title: this.data.taskList[taskListIndex].machine_name,
+      machine_code: this.data.taskList[taskListIndex].machine_code
+    });
 
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+  // modal层传来的cancel事件
+  modalCancel() {
+    console.log('material cancel');
   },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
+  // modal层传来的confirm事件
+  modalConfirm() {
 
   }
+
 })
